196c2825840de2b58468b070463f6463a2e337cc
wangqiang <king74264@outlook.com>
2026-09-17 09:11:36 +0800

    联系人关系查询

--- src/packages/contact/pages/info/modules/PageV.vue
+++ src/packages/contact/pages/info/modules/PageV.vue
@@ -4,30 +4,34 @@
 
     const {form} = useForm(null, {id: "contact-info"});
 
-    const getRelations = async () => {
-        uni.showLoading({mask: true});
+    const {data: relations, send: getRelations} = useRequest(
+        () => {
+            return Apis.contactRelation.list({pathParams: {contactId: form.value.contact.id}});
+        },
+        {
+            immediate: false,
+            initialData: [],
+            middleware: async (_, next) => {
+                uni.showLoading({mask: true});
 
-        try {
-            const {data} = await Apis.contactRelation.list({pathParams: {contactId: form.value.contact.id}});
+                try {
+                    const {data} = await next();
 
-            console.log("relations", data);
-        } finally {
-            uni.hideLoading();
+                    return data;
+                } finally {
+                    uni.hideLoading();
+                }
+            }
         }
-    };
+    );
 
     const onCreateRelation = () => {
-        const to = resolvePage({
-            name: "ContactCreateRelation",
-            params: {contactId: form.value.contact.id}
-        });
+        const to = resolvePage({name: "ContactCreateRelation", params: {contactId: form.value.contact.id}});
 
         uni.navigateTo({
             url: to.fullPath,
             events: {
-                "reload:data": () => {
-                    console.log("reload:data");
-                }
+                "reload:data": getRelations
             }
         });
     };
@@ -42,64 +46,47 @@
             <view class="ml-19.08rpx text-22.90rpx c-primary6 lh-38.17rpx">关系情况</view>
             <view class="ml-auto text-19.08rpx c-#2F59F4 lh-38.17rpx" @click="onCreateRelation()">添加关系</view>
         </view>
-        <view class="lh-38.17rpx">
-            <view class="mx-19.08rpx b-b-(1px primary6/10 solid) bg-#ffffff p-19.08rpx last:b-b-none">
+        <view v-if="relations.length" class="lh-38.17rpx">
+            <view
+                v-for="item in relations"
+                :key="item.id"
+                class="mx-19.08rpx b-b-(1px primary6/10 solid) bg-#ffffff p-19.08rpx last:b-b-none"
+            >
                 <view class="flex items-center">
-                    <view class="text-22.90rpx">张女士</view>
-                    <view class="ml-11.45rpx text-19.08rpx c-primary6/50">母亲</view>
+                    <view class="text-22.90rpx">{{ item.name }}</view>
+                    <view class="ml-11.45rpx text-19.08rpx c-primary6/50">{{ item.relation }}</view>
                     <view class="i-ri:edit-box-line ml-auto size-30.53rpx c-primary6" />
                     <view class="i-ri:close-large-line ml-30.53rpx size-30.53rpx c-primary6" />
                 </view>
                 <view class="flex items-center">
                     <view class="i-ri:cake-fill size-19.08rpx c-#FBC050" />
-                    <view class="ml-11.45rpx text-19.08rpx c-primary6/50">1955年01月23日</view>
+                    <view class="ml-11.45rpx text-19.08rpx c-primary6/50">
+                        {{ formatDate(new Date(item.birthday), "yyyy年MM月dd日") }}
+                    </view>
                 </view>
                 <view class="mt-19.08rpx">
                     <view class="flex flex-wrap -m-4.77rpx">
                         <view v-for="i in 6" :key="i" class="m-4.77rpx">
-                            <wd-tag size="small" type="primary" custom-class="!block">奶油蛋糕-{{ i }}</wd-tag>
+                            <wd-tag custom-class="!block" size="small" type="primary">奶油蛋糕-{{ i }}</wd-tag>
                         </view>
                     </view>
                 </view>
                 <view class="mt-19.08rpx">
                     <view class="flex flex-wrap -m-4.77rpx">
                         <view class="m-4.77rpx">
-                            <wd-tag size="small" type="primary" variant="light" custom-class="!block">橘子香蕉</wd-tag>
+                            <wd-tag custom-class="!block" size="small" type="primary" variant="light">橘子香蕉</wd-tag>
                         </view>
                         <view class="m-4.77rpx">
-                            <wd-tag size="small" type="primary" variant="light" custom-class="!block">FPS游戏</wd-tag>
+                            <wd-tag custom-class="!block" size="small" type="primary" variant="light">FPS游戏</wd-tag>
                         </view>
                     </view>
                 </view>
             </view>
-            <view class="mx-19.08rpx b-b-(1px primary6/10 solid) bg-#ffffff p-19.08rpx last:b-b-none">
-                <view class="flex items-center">
-                    <view class="text-22.90rpx">张女士</view>
-                    <view class="ml-11.45rpx text-19.08rpx c-primary6/50">母亲</view>
-                    <view class="i-ri:edit-box-line ml-auto size-30.53rpx c-primary6" />
-                    <view class="i-ri:close-large-line ml-30.53rpx size-30.53rpx c-primary6" />
-                </view>
-                <view class="flex items-center">
-                    <view class="i-ri:cake-fill size-19.08rpx c-#FBC050" />
-                    <view class="ml-11.45rpx text-19.08rpx c-primary6/50">1955年01月23日</view>
-                </view>
-                <view class="mt-19.08rpx">
-                    <view class="flex flex-wrap -m-4.77rpx">
-                        <view v-for="i in 6" :key="i" class="m-4.77rpx">
-                            <wd-tag size="small" type="primary" custom-class="!block">奶油蛋糕-{{ i }}</wd-tag>
-                        </view>
-                    </view>
-                </view>
-                <view class="mt-19.08rpx">
-                    <view class="flex flex-wrap -m-4.77rpx">
-                        <view class="m-4.77rpx">
-                            <wd-tag size="small" type="primary" variant="light" custom-class="!block">橘子香蕉</wd-tag>
-                        </view>
-                        <view class="m-4.77rpx">
-                            <wd-tag size="small" type="primary" variant="light" custom-class="!block">FPS游戏</wd-tag>
-                        </view>
-                    </view>
-                </view>
+        </view>
+        <view v-else class="p-[76.34rpx_0_57.25rpx]">
+            <view class="flex flex-col items-center">
+                <view class="i-icon-park-outline:termination-file size-76.34rpx c-primary6/10" />
+                <view class="mt-19.08rpx text-22.90rpx c-primary6/50 lh-38.17rpx">未查询到关系情况信息</view>
             </view>
         </view>
     </view>
